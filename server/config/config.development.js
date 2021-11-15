3f261d38a11d1b134bd681c1bff8dc4996476c69
MisterZik <misterzik@users.noreply.github.com>
2021-11-14 19:13:18 -0500

    UP-3.1.8

--- server/config/config.development.js
+++ server/config/config.development.js
@@ -14,8 +14,22 @@ config.hostname = 'dev.example.com';
 config.port = '8080';
 
 // Settings :: MongoDB
+
 config.mongo_isEnabled = true;
 config.mongo = {};
 config.mongo.uri = process.env.MONGO_URI || 'localhost';
 config.mongo.port = '';
 config.mongo.db = 'myFirstDatabase';
+
+// Settings :: SWAPI Sample
+
+config.swapi_isEnabled = true;
+config.swapi = {};
+config.swapi.uri = 'https://swapi.dev/api/people/';
+config.swapi.configs = {
+    method: "GET",
+    url: "",
+    headers: {
+      "Content-Type": "application/json"
+    }
+};
