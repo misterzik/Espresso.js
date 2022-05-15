f98d57923f60b99c680ad7434bbc0d6f164d0589
MisterZik <misterzik@users.noreply.github.com>
2022-05-15 16:21:30 -0400

    (UPG)APIroutes-v3.1.13

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,13 +7,13 @@
  */
 
 const express = require("express");
-const cfg = require("./../../../server");
-const router = express.Router();
 const axios = require("axios");
+const router = express.Router();
+const configuration = require("../../server");
 
 const getAPI = (req, res) => {
   axios
-    .get(cfg.swapi.uri, cfg.swapi.configs)
+    .get(configuration.swapi.uri, configuration.swapi.configs)
     .then(function (response) {
       if (response.status == 200) {
         res.json(response.data);
@@ -26,7 +26,7 @@ const getAPI = (req, res) => {
 
 const getItem = (req, res, params) => {
   axios
-    .get(cfg.swapi.uri + "/" + params, cfg.swapi.configs)
+    .get(configuration.swapi.uri + "/" + params, configuration.swapi.configs)
     .then(function (response) {
       if (response.status == 200) {
         res.json(response.data);
@@ -39,7 +39,10 @@ const getItem = (req, res, params) => {
 
 const getItemId = (req, res, params, paramsId) => {
   axios
-    .get(cfg.swapi.uri + params + "/" + paramsId + "/", cfg.swapi.configs)
+    .get(
+      configuration.swapi.uri + params + "/" + paramsId + "/",
+      configuration.swapi.configs
+    )
     .then(function (response) {
       if (response.status == 200) {
         res.json(response.data);
@@ -49,15 +52,12 @@ const getItemId = (req, res, params, paramsId) => {
     })
     .catch((err) => res.send(err));
 };
-
 router.get("/v1/", (req, res) => {
   getAPI(req, res);
 });
-
 router.get("/v1/:item", (req, res, next) => {
   getItem(req, res, req.params.item);
 });
-
 router.get("/v1/:item/:itemId", (req, res, next) => {
   getItemId(req, res, req.params.item, req.params.itemId);
 });
--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,20 @@
  */
 
 module.exports = (app) => {
-  const cfg = require("./../../server"),
-    path = require("path"),
+  const configuration = require("../server"),
+    Path = require("path"),
     api = require("./api");
 
   app.get("/", function (res) {
-    res.sendFile("index.html", { root: path.join("./public") });
+    res.sendFile("index.html", { root: Path.join("./public") });
   });
 
-  if (cfg.swapi_isEnabled == true) {
+  if (configuration.swapi_isEnabled == true) {
     app.use("/api", api);
   }
 
   require("./db")(app);
-  require("./../utils/global.message")(app);
+  require("../server/utils/global.message")(app);
 
   app.use(function (req, res, next) {
     res.status(404).send("404 - Sorry can't find that!");
